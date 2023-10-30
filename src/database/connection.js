110ce463ddd9bd3cee76fb904d22e7742ce72bfb
Leonardo Gomes <leonrworker@gmail.com>
2023-10-30 17:41:50 -0300

    create connection config

---
+++ src/database/connection.js
@@ -0,0 +1,17 @@
+const postgres = require('postgres')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const connection = postgres({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: 'require',
+  connection: {
+    options: `project=${process.env.ENDPOINT_ID}`
+  }
+})
+
+module.exports = connection
