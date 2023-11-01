0d99b576656141b0b9cabfdb91b3a49ef9d0b704
Leonardo Gomes <leonrworker@gmail.com>
2023-11-01 20:23:58 -0300

    remove unecessary controllers

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,7 @@ const AdminController = require('./controllers/AdminController')
 const UserController = require('./controllers/UserController')
 const AdminSessionController = require('./controllers/AdminSessionController')
 const UserSessionController = require('./controllers/UserSessionController')
-const ItemController = require('./controllers/ItemController')
-const CategoryController = require('./controllers/CategoryController')
-const ToGiveBackController = require('./controllers/ToGiveBackController')
+const LoansController = require('./controllers/LoansController')
 
 routes.post('/admin', AdminController.create)
 routes.get('/admin', AdminController.index)
@@ -24,23 +22,9 @@ routes.delete('/users/:id', UserController.delete)
 routes.post('/user-session', UserSessionController.create)
 routes.post('/admin-session', AdminSessionController.create)
 
-routes.post('/categories', CategoryController.create)
-routes.get('/categories', CategoryController.index)
-routes.get('/categories/:id', CategoryController.show)
-routes.put('/categories/:id', CategoryController.update)
-routes.delete('/categories/:id', CategoryController.delete)
-
-routes.post('/items', ItemController.create)
-routes.get('/items', ItemController.index)
-routes.get('/items/:id', ItemController.show)
-routes.put('/items/:id', ItemController.update)
-routes.delete('/items/:id', ItemController.delete)
-
-routes.post('/giveback', ToGiveBackController.create)
-routes.get('/giveback', ToGiveBackController.index)
-routes.get('/giveback/:id', ToGiveBackController.show)
-routes.put('/giveback/:id', ToGiveBackController.update)
-routes.delete('/giveback/:id', ToGiveBackController.delete)
-
+routes.post('/loans', LoansController.create)
+routes.get('/loans', LoansController.index)
+routes.get('/loans/:id', LoansController.show)
+routes.put('/loans/:id', LoansController.update)
 
 module.exports = routes
